c71e0346bafa9d9ca00391522821657c0e196dca
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-01-25 14:35:58 +0200

    make Page Title into reusable component

--- app/job-search/page.tsx
+++ app/job-search/page.tsx
@@ -1,5 +1,15 @@
+import PageTitle from "@/components/PageTitle";
+
 const JobSearch = () => {
-  return <div>JobSearch</div>;
+  return (
+    <div className="flex flex-1 flex-col items-center px-6 py-7 md:py-12">
+      <div className="flex w-full max-w-[80rem] flex-col gap-10 xl:flex-row">
+        <div className="flex flex-col">
+          <PageTitle text="Let’s find your dream job" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobSearch;
